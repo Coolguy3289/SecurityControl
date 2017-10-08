d391dfc451ce21b9b086e7877642d158275bedf4
Coolguy3289 <blackbirdralph@aol.com>
2017-10-07 22:02:49 -0400

    Added SQLite Insertion form to Home page. Tested Values are saving.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,8 +12,25 @@ export class HomePage {
   constructor(public navCtrl: NavController, private sqlite: SQLite) {
 
   }
-  SystemSubmission(){
+  SystemSubmission(Name, Number){
+    console.log(Name);
+    console.log(Number);
+    this.sqlite.create({
+      name: 'data.db',
+      location: 'default'
+    })
+      .then((db: SQLiteObject) => {
 
+
+        db.executeSql('create table securitySystems(systemName VARCHAR(32), systemNumber INT(10))', {})
+          .then(() => console.log('Executed SQL Creation/Opening'))
+          .catch(e => console.log(e));
+        db.executeSql("insert into securitySystems(systemName, systemNumber) values("+Name+", "+Number+")", {})
+          .then(() => console.log('Inserting Data into SQLite File'))
+          .catch(e => console.log(e));
+
+      })
+      .catch(e => console.log(e));
 
 
   }
